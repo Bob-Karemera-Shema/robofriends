0d2ca021e721cb9a72e6706caa20a39dc20e0563
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2023-09-06 14:34:07 +0100

    UI design and serach event handling

---
+++ src/CardList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from './Card';
+
+class CardList extends React.Component {
+    render() {
+        const { robots } = this.props;
+        return (
+            <div>
+                {
+                    robots.map((robot, i) => {
+                        return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email}/>;
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default CardList;
