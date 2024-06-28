94b12d94f61281b4612fa4b1a8ce2223f021fba9
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2024-06-28 11:18:22 +0100

    searchRobots reducer and action

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 
+// const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
